f77ef1e4f213383da4dc4de6f20a43c4259e646e
janc18 <josecla011@gmail.com>
2023-07-27 20:32:43 -0600

    add: c file for test communication through ioctl based in EmbeTronicX example

---
+++ src/test-kernel-modules/i2c_mpu6050/read_value_iotcl.c
@@ -0,0 +1,46 @@
+/***************************************************************************/ /**
+                                                                               *  \file       test_app.c
+                                                                               *
+                                                                               *  \details    Userspace application to test the Device driver
+                                                                               *
+                                                                               *  \author     EmbeTronicX
+                                                                               *
+                                                                               *  \Tested with Linux raspberrypi 5.10.27-v7l-embetronicx-custom+
+                                                                               *  \Tested with Linux raspberrypi 5.15.84+
+                                                                               *******************************************************************************/
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/ioctl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+// ioctl
+#define GX _IOR('a', 'a', int16_t *)
+#define GY _IOR('a', 'b', int16_t *)
+#define GZ _IOR('a', 'c', int16_t *)
+#define AX _IOR('a', 'd', int16_t *)
+#define AY _IOR('a', 'e', int16_t *)
+#define AZ _IOR('a', 'f', int16_t *)
+
+unsigned int ks[6] = {GX, GY, GZ, AX, AY, AZ};
+
+int fd;
+int main() {
+  int32_t value, number;
+  fd = open("/dev/mpu6050", O_RDWR);
+  printf("\nOpening Driver\n");
+  if (fd < 0) {
+    printf("Cannot open device file...\n");
+    return 0;
+  }
+
+  for (int i = 0; i < 6; i++) {
+    ioctl(fd, ks[i], (int32_t *)&value);
+    printf("Value %d is %d\n", i, value);
+    usleep(18000);
+  }
+  printf("Closing Driver\n");
+  close(fd);
+}
