57ff00304863d8b7b5871381e159bb9c382d8e38
janc18 <josecla011@gmail.com>
2023-08-27 17:24:39 -0600

    chore: added function prototypes in the windows.h file

--- src/GUI-GTK3/include/windows.h
+++ src/GUI-GTK3/include/windows.h
@@ -1,7 +1,13 @@
 #ifndef _WINDOWS_H_
 #define _WINDOWS_H_
 #include <gtk/gtk.h>
+#include "device.h"
+#include "app.h"
 void close_test_window(GtkWidget *,gpointer data);
 void close_message(GtkWidget *,gpointer data);
 void open_test_window(GtkWidget *,gpointer data);
+void activate_elements(ObjectsUI *UI, Device *car);
+void deactivate_elements(ObjectsUI *UI);
+void start_timer(gpointer data);
+gboolean UpdateLabelCountdown(gpointer data);
 #endif
