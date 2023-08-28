26702d805d604fbbcd8cf9226565be2f2d9631c7
janc18 <josecla011@gmail.com>
2023-08-28 17:18:09 -0600

    chore: format windows.h file and added another two prototypes functions

--- src/GUI-GTK3/include/windows.h
+++ src/GUI-GTK3/include/windows.h
@@ -1,13 +1,15 @@
 #ifndef _WINDOWS_H_
 #define _WINDOWS_H_
-#include <gtk/gtk.h>
-#include "device.h"
 #include "app.h"
-void close_test_window(GtkWidget *,gpointer data);
-void close_message(GtkWidget *,gpointer data);
-void open_test_window(GtkWidget *,gpointer data);
+#include "device.h"
+#include <gtk/gtk.h>
+void close_test_window(GtkWidget *, gpointer data);
+void close_message(GtkWidget *, gpointer data);
+void open_test_window(GtkWidget *, gpointer data);
 void activate_elements(ObjectsUI *UI, Device *car);
 void deactivate_elements(ObjectsUI *UI);
 void start_timer(gpointer data);
 gboolean UpdateLabelCountdown(gpointer data);
+void starting_test(gpointer data);
+void print_status_text(gpointer data, gchar *buffer);
 #endif
